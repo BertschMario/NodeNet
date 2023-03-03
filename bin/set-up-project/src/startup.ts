6577725080f9512bf9406c01f718701b4b744a46
Mario Bertsch <83945558+BertschMario@users.noreply.github.com>
2023-03-03 15:25:42 +0100

    Update 0.2.5 | Swagger Update

--- bin/set-up-project/src/startup.ts
+++ bin/set-up-project/src/startup.ts
@@ -9,7 +9,14 @@ export class AppModule {
   
   config: ServerConfig = {
     name: 'NodeNet Server',
-    port: 3000
+    port: 3000,
+    // host: 'http://localhost', -- Host of your server application (default: http://localhost)
+    // version: '1.0.0', -- Version of your server application
+    swagger: { // If not defined, Swagger will not be enabled
+      path: '/api', // Path to Swagger (e.g. host:port/api)
+      // title: 'NodeNet API', -- Title displayed in Swagger
+      // description: 'NodeNet server API', -- Description displayed in Swagger
+    }
   };
   
   init(){ }
